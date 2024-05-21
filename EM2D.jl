1e55413ea42648841f5bd80be28c04d1e9aea9bf
Sfiguz7 <37039432+Sfiguz7@users.noreply.github.com>
2024-05-21 15:48:05 +0200

    Small cleanup of main file

--- EM2D.jl
+++ EM2D.jl
@@ -1,26 +1,15 @@
-#
-# sets input
-#
-
-input_file="demo2.jl"
-#input_file="demo3.jl"
+input_file="hardening.jl"
 
 using SparseArrays
 using LinearAlgebra
 using Arpack
 using ExtendableSparse
-#using BenchmarkTools
 using Pardiso
 using MAT
 using MATLAB
-#using Plots
 
 include("./source/EM2D_defs.jl")
 include("./source/EM2D_globalprocedure.jl")
-#include("./source/EM2D_globalprocedure2.jl")
-#include("./source/EM2D_globalprocedure3.jl")
-#include("./source/EM2D_globalprocedure4.jl")
-#include("./source/EM2D_globalprocedure8.jl")
 include("./source/EM2D_readgmsh.jl")
 include("./source/EM2D_outgmsh.jl")
 include("./source/EM2D_elemental.jl")
@@ -29,7 +18,6 @@ include("./source/EM2D_eig.jl")
 include("./source/EM2D_param_struct.jl")
 include("./source/EM2D_dpim.jl")
 include("./source/EM2D_dpim_FEM.jl")
-#include("./source/EM2D_prove.jl")
 include("./source/EM2D_dpim_realification.jl")
 include("./source/EM2D_dpim_output.jl")
 include("./source/quadrature.jl")
@@ -37,10 +25,6 @@ include("./source/shape_functions.jl")
 include("./source/EM2D_matcont.jl")
 
 globalprocedure()
-#globalprocedure2()
-#globalprocedure3()
-#globalprocedure4()
-#globalprocedure8()
 
 println("done!")
 
